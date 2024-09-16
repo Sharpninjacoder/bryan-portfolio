9716859247916cd6b736443fca8cb03a7dc2d94f
Bryan Turner <sharpninjacoder@gmail.com>
2024-09-16 13:30:46 -0600

    initial commit

---
+++ utils/data/educations.js
@@ -0,0 +1,8 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor's degree, Computer Science",
+    duration: "2010 - 2014",
+    institution: "Clemson University",
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Nicholas Paolo",
+  profile: '/profile.jpeg',
+  designation: "Software Developer",
+  description: "My name is Nicholas Paolo. I am a highly skilled and experienced software developer with a deep understanding of software design, development, and maintenance. I am proficient in multiple programming languages and technologies, and I have a proven track record of delivering high-quality software solutions. I am a strong team player and I am always eager to learn new things. Ultimately, the goal is to create software that has a positive impact on the world, solving problems, increasing efficiency, and making people's lives easier. I am confident that I can make a significant contribution to your organization.",
+  email: 'nicholaspaolo9@gmail.com',
+  phone: '+1 872 272 7217',
+  address: '1057 N Saguaro ST, Chandler, Arizona, United States',
+  github: 'https://github.com/Nicholas-dev-ui',
+  linkedIn: 'https://www.linkedin.com/in/nicholas-paolo-700953325/',
+  stackOverflow: 'https://stackoverflow.com/users/25537821/zbr',
+  facebook: 'https://www.facebook.com/zbr.constantin/',
+  twitter: 'https://twitter.com/zbrconstatin',
+  leetcode: "https://leetcode.com/u/BrainArtfu/",
+  devUsername: "Benjamin.Hernandez2645",
+  resume: "https://drive.google.com/file/d/1WKEBIECCZd6y3qXM8XvCjZ3LQ0D6G3PG/view?usp=sharing"
+}
